bdd83e7db3e07b74a2b2de271c8aa6f8dfcfe0ae
kahfi <azanjabiil@gmail.com>
2020-06-09 16:25:19 +0700

    Game yang pertama untuk game jam

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1591694596,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite2-sheet1.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"images/sprite6-sheet0.png",
+		"media/06_game over.ogg",
+		"media/05_ending.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
